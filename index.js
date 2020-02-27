d67db5c153a31310d305d45ca665c7cc7017e4d9
Asher <asher_3@hotmail.co.uk>
2020-02-27 10:35:55 +0000

    finishing touches

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ window.addEventListener('load', () => {
 		pad.addEventListener('click', function() {
 			sounds[index].currentTime = 0;
 			sounds[index].play();
+
+			createBubbles(index);
 		});
 	});
+
+	const createBubbles = (index) => {
+		const bubble = document.createElement('div');
+		visual.appendChild(bubble);
+		bubble.style.backgroundColor = colors[index];
+		bubble.style.animation = 'jump 1s ease';
+		bubble.addEventListener('animationend', function() {
+			visual.removeChild(this);
+		});
+	};
 });
