06bc80a4cce1c83d26fdef2e1d3ea7f9ff81501c
Asher <asher_3@hotmail.co.uk>
2020-02-27 09:31:14 +0000

    added js file and styles

---
+++ index.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+	const sounds = document.querySelectorAll('.sound');
+	const pads = document.querySelectorAll('.pads div');
+	const visual = document.querySelector('.visual');
+	const colors = [ '#60d394', '#d36060', '#c060d3', '#d3d160', '#6860d3' ];
+
+	//Legs get going with the sound here
+	pads.forEach((pad, index) => {
+		pad.addEventListener('click', function() {
+			sounds[index].currentTime = 0;
+			sounds[index].play();
+		});
+	});
+});
